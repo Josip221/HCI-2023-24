519ff4438e206a7b8e1004c0f054dff8bdaef210
Josip Maretić <josip.maretic2011@outlook.com>
2023-10-20 12:45:51 +0200

    project 3

---
+++ class-projects/class-project-3/app/cart/order/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="flex justify-center flex-col items-center gap-10 m-10 border-2 border-solid p-10 rounded-md">
+      <h1 className="font-bold text-2xl">Order Page</h1>
+
+      <Link className="underline underline-line-offset-4" href="/">
+        Go to Home Page
+      </Link>
+    </div>
+  );
+}
