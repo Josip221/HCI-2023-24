d6519f58376c09d05dcb9fe18d324ef2f92f0346
Josip Maretić <josip.maretic2011@outlook.com>
2023-12-18 21:43:22 +0100

    fixed hamburger and 404 page

--- semester-project/components/Hamburger.tsx
+++ semester-project/components/Hamburger.tsx
@@ -12,7 +12,7 @@ function Hamburger() {
     <div className=" hover:cursor-pointer">
       <div
         onClick={toggleMenu}
-        className="m-5 md:hidden lg:hidden flex flex-col gap-1 justify-around"
+        className=" md:hidden lg:hidden flex flex-col gap-1 justify-around"
       >
         <div className="transition-all origin-center w-6 h-1 bg-white"></div>
         <div className="transition-all origin-center w-6 h-1 bg-white"></div>
@@ -21,9 +21,9 @@ function Hamburger() {
       <div
         className={` ${
           isMenuOpen ? "absolute" : "hidden"
-        }  w-full top-10 left-0 bg-[#336688ff] h-30 z-10`}
+        }  flex flex-col justify-center items-center w-full top-15 left-0 bg-[#336688ff] h-40 z-10 `}
       >
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center ">
           <Link href="/products">Products</Link>
           <Link href="/cart">Cart</Link>
           <Link href="/about">About Us</Link>
--- semester-project/components/ItemCard.tsx
+++ semester-project/components/ItemCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Rating from "@mui/material/Rating";
-import Spinner from "@/components/Spinner";
+import Link from "next/link";
 
 interface ItemCardProps {
   image: string;
@@ -12,20 +12,22 @@ interface ItemCardProps {
 function ItemCard({ image, title, price, weight }: ItemCardProps) {
   const randomNum = Math.random() * (4.8 - 2.5) + 2.5;
   return (
-    <div className="flex flex-col justify-center items-center border-2 border-[#6c757d] my-2 mx-5 p-3">
-      <img
-        className="border-2 border-[#6c757d] h-60 object-contain "
-        src={image}
-      />
-      <div className="m-2">{title}</div>
+    <Link href="/products/category/product/1">
+      <div className="flex flex-col justify-center items-center border-2 border-[#6c757d] my-2 mx-5 p-3">
+        <img
+          className="border-2 border-[#6c757d] h-60 object-contain "
+          src={image}
+        />
+        <div className="m-2">{title}</div>
 
-      <Rating name="read-only" value={randomNum} readOnly />
-      <div className="m-2">
-        <span className="align-top  text-xs">€</span>
-        <span className="text-lg">{price}</span>
-        <span className="align-top text-xs">99</span>
+        <Rating name="read-only" value={randomNum} readOnly />
+        <div className="m-2">
+          <span className="align-top  text-xs">€</span>
+          <span className="text-lg">{price}</span>
+          <span className="align-top text-xs">99</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
